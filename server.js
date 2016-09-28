c16438c1a16f35b9c1eac6d9b8aa3150ce64cd15
neethuneetz <neethualayilnarayanan@gmail.com>
2016-09-28 07:32:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("article-one request will be served here");
+  res.sendFile(path.join(__dirname,'ui','article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("article-two request will be served here");
+  res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("article-three request will be served here");
+  res.sendFile(path.join(__dirname,'ui','atricle-three.html'));
 });
 
 
