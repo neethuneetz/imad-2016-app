eb55cb10ec5751053130fcfc3fec61c2fa16dcb2
neethuneetz <neethualayilnarayanan@gmail.com>
2016-09-30 09:09:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var img=document.getElementById('madi');
 var marginleft=0;
 
 function moveRight(){
-    marginLeft=marginleft+10;
-    img.style.marginLeft=marginLeft+'px';
+    marginLeft = marginleft+10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
